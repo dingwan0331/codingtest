2c5d0d356b9c797dc21bf969c16910c4136c8230
dingwan <dingwan0331@gmail.com>
2022-09-23 21:23:16 +0900

    SQL: 이름이 있는 동물의 아이디

---
+++ sql/programmers/59407.sql
@@ -0,0 +1,8 @@
+/* 이름이 있는 동물의 아이디 */
+
+/*
+problem url : https://school.programmers.co.kr/learn/courses/30/lessons/59407
+*/
+
+
+SELECT ANIMAL_ID from ANIMAL_INS WHERE NAME is NOT NULL ORDER BY ANIMAL_ID ASC
